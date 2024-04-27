b3e28ab20bbf02ce249d77c39c7602618487b793
Luciano Barros <lucianof.barros89@gmail.com>
2024-04-27 19:01:00 -0300

    Configurando mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,21 @@
-import express from 'express';
-import { PORT, mongoDBURL } from './config.js';
-import mongoose from 'mongoose';
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.get('/', (request, response) => {
-    console.log(request);
-    return response.status(234).send('Welcome to server.')
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to server.");
 });
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
-});
 
-mongoose.connect(mongoDBURL);
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database.');
+    app.listen(PORT, () => {
+        console.log(`App is listening to port: ${PORT}`);
+      });
+})
+  .catch((error) => {console.log(error)});
