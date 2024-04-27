c93c7a9a5b6548efd96f0d24991e2742e02c46a3
Luciano Barros <lucianof.barros89@gmail.com>
2024-04-27 18:54:17 -0300

    1st commit
    Inicialização do server local,
    configuração da database mongodb

---
+++ backend/config.sample.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://admin:<password>@books-store-mern.1uviiam.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN';
---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome to server.')
+});
+
+app.listen(PORT, () => {
+    console.log(`App is listening to port: ${PORT}`);
+});
+
+mongoose.connect(mongoDBURL);
